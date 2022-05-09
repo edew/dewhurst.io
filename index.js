4b1cf23d1fe52474b1916fc8b9606c34bf69a56a
Edward Dewhurst <edward@dewhurst.io>
2022-05-09 22:06:32 +0100

    Get post date from git commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs/promises');
 const os = require('os');
+const childProcess = require('child_process');
 const hogan = require('hogan.js');
 const marked = require('marked');
 
@@ -35,19 +36,39 @@ async function readPosts(postsDirectoryPath) {
   const postPaths = await fs.readdir(postsDirectoryPath);
   const posts = [];
 
-  postPaths.sort().reverse();
-
-  for (const postPath of postPaths) {
-    const postFileContent = await fs.readFile(path.join(postsDirectoryPath, postPath), { encoding: 'utf8' });
-    posts.push(postFromString(postFileContent));
+  for (const relativePostPath of postPaths) {
+    const absolutePostPath = path.resolve(postsDirectoryPath, relativePostPath);
+    const postFileContent = await fs.readFile(absolutePostPath, { encoding: 'utf8' });
+    const isoDateString = await readMostRecentCommitDateForFile(absolutePostPath);
+    posts.push(postFromString(postFileContent, isoDateString));
   }
+
+  // newest first
+  posts.sort((a, b) => b.timestamp - a.timestamp);
 
   return posts;
 }
 
+function readMostRecentCommitDateForFile(filePath) {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(`git log --format="%aI" -n 1 ${path.resolve(filePath)}`, (err, stdout) => {
+      if (err) {
+        reject(err);
+      }
+  
+      resolve(stdout.split(os.EOL).filter(x => x !== '')[0]);
+    });
+  });
+}
+
 async function writePosts(posts) {
+  const links = [
+    { href: '/', text: 'All posts' },
+  ];
+  
   for await (const post of posts) {
-    const postHtml = await renderTemplate('post', { blogTitle, post });
+    const data = { blogTitle, post, links };
+    const postHtml = await renderTemplate('post', data);
     const postHtmlPath = path.join(buildDirectoryPath, post.path);
     await fs.mkdir(postHtmlPath);
     await fs.writeFile(path.join(postHtmlPath, 'index.html'), postHtml);
@@ -85,7 +106,7 @@ function snakeCase(string) {
   return string.replaceAll(/\s/g, '_').replaceAll(/\W/g, '').toLowerCase();
 }
 
-function postFromString(string) {
+function postFromString(string, isoDateString) {
   const markdownTokens = marked.marked.lexer(string);
   const titleToken = markdownTokens.find(x => x.type === 'heading');
 
@@ -93,10 +114,13 @@ function postFromString(string) {
     throw new Error('blimey this post dont got no title mate');
   }
 
+  const postDate = new Date(isoDateString);
   return {
     title: titleToken.text,
     path: snakeCase(titleToken.text),
-    html: marked.marked.parser(markdownTokens), 
+    html: marked.marked.parser(markdownTokens),
+    timestamp: postDate.getTime(),
+    dateString: postDate.toLocaleString('en-GB', { month: 'long', year: 'numeric', day: 'numeric' }),
   }
 }
 
--- tests/postFromString.test.js
+++ tests/postFromString.test.js
@@ -8,33 +8,52 @@ function test(f) {
 }
 
 test(function should_create_post_with_title_and_path() {
-  const actual = postFromString('# I love to write useless software for fun');
+  const actual = postFromString('# I love to write useless software for fun', '1970-01-01T00:00:00.000Z');
   const expected = {
     title: 'I love to write useless software for fun',
     path: 'i_love_to_write_useless_software_for_fun',
-    html: '<h1 id="i-love-to-write-useless-software-for-fun">I love to write useless software for fun</h1>\n'
+    html: '<h1 id="i-love-to-write-useless-software-for-fun">I love to write useless software for fun</h1>\n',
+    timestamp: 0,
+    dateString: '1 January 1970',
   };
 
   assert.deepStrictEqual(actual, expected);
 });
 
 test(function should_create_post_with_title_path_and_one_paragraph() {
-  const actual = postFromString('# I love to write useless\n\nsoftware for fun');
+  const actual = postFromString('# I love to write useless\n\nsoftware for fun', '1970-01-01T00:00:00.000Z');
   const expected = {
     title: 'I love to write useless',
     path: 'i_love_to_write_useless',
-    html: '<h1 id="i-love-to-write-useless">I love to write useless</h1>\n<p>software for fun</p>\n'
+    html: '<h1 id="i-love-to-write-useless">I love to write useless</h1>\n<p>software for fun</p>\n',
+    timestamp: 0,
+    dateString: '1 January 1970',
   };
 
   assert.deepStrictEqual(actual, expected);
 });
 
 test(function should_create_post_with_title_path_and_multiple_paragraphs() {
-  const actual = postFromString('# I love to write useless\n\nsoftware for fun\n\ntake me to afternoon tea!');
+  const actual = postFromString('# I love to write useless\n\nsoftware for fun\n\ntake me to afternoon tea!', '1970-01-01T00:00:00.000Z');
   const expected = {
     title: 'I love to write useless',
     path: 'i_love_to_write_useless',
-    html: '<h1 id="i-love-to-write-useless">I love to write useless</h1>\n<p>software for fun</p>\n<p>take me to afternoon tea!</p>\n'
+    html: '<h1 id="i-love-to-write-useless">I love to write useless</h1>\n<p>software for fun</p>\n<p>take me to afternoon tea!</p>\n',
+    timestamp: 0,
+    dateString: '1 January 1970',
+  };
+
+  assert.deepStrictEqual(actual, expected);
+});
+
+test(function should_create_a_post_with_timestamp_and_datestring() {
+  const actual = postFromString('# wow', '2022-04-28T22:27:46+01:00');
+  const expected = {
+    title: 'wow',
+    path: 'wow',
+    html: '<h1 id="wow">wow</h1>\n',
+    timestamp: 1651181266000,
+    dateString: '28 April 2022',
   };
 
   assert.deepStrictEqual(actual, expected);
